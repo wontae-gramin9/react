05042c3b1e418df8e7bc4809ef51e64a2e2b7c03
Wontae Choi <>
2024-07-22 17:01:02 +0900

    [NavLink] Link와 다르게 active class를 토글링을 기본으로 가지고 있음

---
+++ 17-worldwise/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="product" element={<Product />}></Route>
+        <Route path="pricing" element={<Pricing />}></Route>
+        <Route path="*" element={<PageNotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ 17-worldwise/src/pages/PageNotFound.jsx
@@ -0,0 +1,9 @@
+import PageNav from "../components/PageNav";
+export default function PageNotFound() {
+  return (
+    <div>
+      <PageNav />
+      PageNotFound
+    </div>
+  );
+}
