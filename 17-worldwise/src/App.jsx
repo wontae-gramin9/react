3758eb0543e05e200970ed80d4ece3f1423a9e13
Wontae Choi <>
2024-07-22 20:00:40 +0900

    [WorldWide Project] Starter 세팅

--- 17-worldwise/src/App.jsx
+++ 17-worldwise/src/App.jsx
@@ -3,6 +3,7 @@ import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Homepage />}></Route>
         <Route path="product" element={<Product />}></Route>
         <Route path="pricing" element={<Pricing />}></Route>
+        <Route path="login" element={<Login />}></Route>
         <Route path="app" element={<AppLayout />}></Route>
         <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
--- 17-worldwise/src/pages/AppLayout.jsx
+++ 17-worldwise/src/pages/AppLayout.jsx
@@ -1,10 +1,12 @@
-import AppNav from "../components/AppNav";
+import Sidebar from "../components/Sidebar";
+import Map from "../components/Map";
+import styles from "./AppLayout.module.css";
 
 export default function AppLayout() {
   return (
-    <div>
-      AppLayout
-      <AppNav></AppNav>
+    <div className={styles.app}>
+      <Sidebar></Sidebar>
+      <Map></Map>
     </div>
   );
 }
