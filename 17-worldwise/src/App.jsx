a3411e15870a45fb88779fadae15b8c366924fd9
Wontae <wontae.gramin9@gmail.com>
2024-07-25 20:52:29 +0900

    [Optimazation] Lazy loading(모듈 동적 import), Code splitting, Suspense

--- 17-worldwise/src/App.jsx
+++ 17-worldwise/src/App.jsx
@@ -1,36 +1,42 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import Product from "./pages/Product";
-import Homepage from "./pages/Homepage";
-import Pricing from "./pages/Pricing";
-import PageNotFound from "./pages/PageNotFound";
-import Login from "./pages/Login";
-import AppLayout from "./pages/AppLayout";
-import CityList from "./components/CityList";
-import City from "./components/City";
+import { CityProvider } from "./contexts/CityContext";
+import { lazy, Suspense } from "react";
+
 import Form from "./components/Form";
 import CountryList from "./components/CountryList";
-import { CityProvider } from "./contexts/CityContext";
+import CityList from "./components/CityList";
+import City from "./components/City";
+import SpinnerFullPage from "./components/SpinnerFullPage";
+
+const Login = lazy(() => import("./pages/Login"));
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 
 function App() {
   return (
     <CityProvider>
       <BrowserRouter>
-        <Routes>
-          <Route index element={<Homepage />} />
-          <Route path="product" element={<Product />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="login" element={<Login />} />
-          <Route path="app" element={<AppLayout />}>
-            {/* Nested router와 <Outlet/>, index(default) route */}
-            <Route index element={<Navigate to="cities" replace />} />
-            <Route path="cities" element={<CityList />} />
-            <Route path="cities/:id" element={<City />} />
-            <Route path="countries" element={<CountryList />} />
-            <Route path="form" element={<Form />} />
-          </Route>
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
+        <Suspense fallback={<SpinnerFullPage />}>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="login" element={<Login />} />
+            <Route path="app" element={<AppLayout />}>
+              {/* Nested router와 <Outlet/>, index(default) route */}
+              <Route index element={<Navigate to="cities" replace />} />
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </CityProvider>
   );
