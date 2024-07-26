360febcad2e6cfff931cf46c5dcc44a8f6592ce8
Wontae <wontae.gramin9@gmail.com>
2024-07-26 19:09:28 +0900

    [Redux devtool] npm으로 패키지 설치해야함, 환율 dispatch이후에 setCurrency ""로 되는 버그 초기값 "USD"로 수정

--- 20-redux-intro/src/store.js
+++ 20-redux-intro/src/store.js
@@ -3,12 +3,15 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 import { thunk } from "redux-thunk";
-
+import { composeWithDevTools } from "redux-devtools-extension";
 const rootReducer = combineReducers({
   // 두 reducer가 담당하는 state가 value값으로 들어가는 새로운 state를 만듦
   account: accountReducer,
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 export default store;
