1b545a0ab487156796a2ab1e310b9bb594a28449
Wontae <wontae.gramin9@gmail.com>
2024-08-03 15:30:28 +0900

    [Data Router] 컴포넌트가 아니고 data router action이라서 훅을 쓰지 못할 때는 (여기서는 useDispatch) 그냥 store.dispatch(action). 단 최후의 수단임 Order이후에 cart 비우기

--- 22-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 22-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import { useSelector } from "react-redux";
-import { getCart } from "../cart/cartSlice";
+import { clearCart, getCart } from "../cart/cartSlice";
 import EmptyCart from "../cart/EmptyCart";
+import store from "../../store";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -32,6 +32,8 @@ export async function action({ request }) {
   if (Object.keys(errors).length > 0) return errors;
 
   const newOrder = await createOrder(order);
+
+  store.dispatch(clearCart());
 
   // 컴포넌트가 아니라 useNavigate Hook 사용불가
   return redirect(`/order/${newOrder.id}`);
