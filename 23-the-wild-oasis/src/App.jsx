1e8352b16629f02acdb6cb7148fec8ad83d98e6e
Wontae <wontae.gramin9@gmail.com>
2024-08-04 20:00:53 +0900

    [StyledComponent] Tagged template literal도 js template literal이므로 js를 쓸 수 있음. 다만 변수를 할당할 때 string으로 할당하면 syntax highlighting을 못 받는데, 그럴 때는 css``로 묶는다. → tag의 prop을 변수로 받을수 있음 (type prop을 변수로 받음)

--- 23-the-wild-oasis/src/App.jsx
+++ 23-the-wild-oasis/src/App.jsx
@@ -1,9 +1,14 @@
 import GlobalStyles from "./styles/GlobalStyles";
+import Heading from "./ui/Heading";
 
 function App() {
   return (
     <>
       <GlobalStyles />
+      <Heading>H1</Heading>
+      <Heading type="h2">H2</Heading>
+      <Heading type="h3">H3</Heading>
+      <Heading type="p">H3</Heading>
     </>
   );
 }
