5e6fddcb349b1c46dde68e711f97d95fe2c02ec9
Wontae <wontae.gramin9@gmail.com>
2024-08-04 20:36:24 +0900

    [StyledComponent] prop으로 받아서 디자인 시스템 만들고 와 React의 defaultProps으로 기본값 vertical로 설정

--- 23-the-wild-oasis/src/App.jsx
+++ 23-the-wild-oasis/src/App.jsx
@@ -1,14 +1,30 @@
 import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Heading>H1</Heading>
-      <Heading as="h2">H2</Heading>
-      <Heading as="h3">H3</Heading>
-      <Heading as="p">H3</Heading>
+      <Row>
+        <Heading>H1</Heading>
+        <Heading as="h2">H2</Heading>
+        <Button
+          variation="secondary"
+          size="small"
+          onClick={() => alert("Check out")}
+        />
+      </Row>
+      <Row type="horizontal">
+        <Heading as="h3">H3</Heading>
+        <Heading as="p">H3</Heading>
+        <Button
+          variation="danger"
+          size="medium"
+          onClick={() => alert("Check out")}
+        />
+      </Row>
     </>
   );
 }
