3acde36a2dc967780f12525f6e2f26bbe66939d7
Wontae <wontae.gramin9@gmail.com>
2024-08-04 21:32:18 +0900

    [Nested Route] Nested Route로 AppLayout 만들고 Sidebar에 Route 연결하기. styled(써드파티 컴포넌트) Third party의 <NavLInk/>를 어떻게 styled component로 스타일링하는지

---
+++ 23-the-wild-oasis/src/ui/AppLayout.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import styled from "styled-components";
+
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  padding: 4rem 4.8rem 6.4rem;
+`;
+
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
+
+export default function AppLayout() {
+  return (
+    <StyledAppLayout>
+      <Header />
+      <Sidebar />
+      {/* main으로 감싸는 이유는 Outlet를 커버하는 Container를 위한 style을 만들기 위해서*/}
+      <Main>
+        <Container>
+          <Outlet />
+        </Container>
+      </Main>
+    </StyledAppLayout>
+  );
+}
