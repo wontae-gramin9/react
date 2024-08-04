8467d8b0b69fbf4eb0651ae67ab676828a80b104
Wontae <wontae.gramin9@gmail.com>
2024-08-04 20:55:23 +0900

    [React Router] Declarative적인 방법과 기본페이지 index, <Navigate>로... 기본페이지의 Page component 불러오기

--- 23-the-wild-oasis/src/App.jsx
+++ 23-the-wild-oasis/src/App.jsx
@@ -1,30 +1,33 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="users" element={<Users />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Account />} />
+
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
       <GlobalStyles />
-      <Row>
-        <Heading>H1</Heading>
-        <Heading as="h2">H2</Heading>
-        <Button
-          variation="secondary"
-          size="small"
-          onClick={() => alert("Check out")}
-        />
-      </Row>
-      <Row type="horizontal">
-        <Heading as="h3">H3</Heading>
-        <Heading as="p">H3</Heading>
-        <Button
-          variation="danger"
-          size="medium"
-          onClick={() => alert("Check out")}
-        />
-      </Row>
     </>
   );
 }
