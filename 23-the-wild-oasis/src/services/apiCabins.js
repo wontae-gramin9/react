ef70903932574e0dd4b5c13dde0344b3be94b222
Wontae <wontae.gramin9@gmail.com>
2024-08-05 13:07:49 +0900

    [ReactQuery] mutation: useMutation과 cache revalidation with onSuccess, access query client with useQueryClient

--- 23-the-wild-oasis/src/services/apiCabins.js
+++ 23-the-wild-oasis/src/services/apiCabins.js
@@ -8,3 +8,13 @@ export async function getCabins() {
   }
   return data;
 }
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error(`Cabin ${id} could not be deleted`);
+  }
+  return data;
+}
