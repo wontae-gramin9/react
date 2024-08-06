31478e85e5aeedeb94c382a076d5edaaf63da4dc
Wontae <wontae.gramin9@gmail.com>
2024-08-06 23:25:14 +0900

    [HigherOrderComponent] Component를 arg로 받아서 enhanced Component를  리턴, 보통 withEnhancement로 이름 컨벤션

--- 24-advanced-react/src/App.js
+++ 24-advanced-react/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { faker } from "@faker-js/faker";
+import withToggles from "./HOC";
 
 const products = Array.from({ length: 20 }, () => {
   return {
@@ -73,7 +74,22 @@ function List({ title, items, render }) {
   );
 }
 
+// LATER: Let's say we got this component from a 3rd-party library,
+// and can't change it.
+// But we still want to add the 2 toggle functionalities to it
+function ProductList({ title, items }) {
+  return (
+    <ul className="list">
+      {items.map((product) => (
+        <ProductItem key={product.productName} product={product} />
+      ))}
+    </ul>
+  );
+}
+
 export default function App() {
+  const ProductListWithToggles = withToggles(ProductList);
+
   return (
     <div>
       <h1>Render Props Demo</h1>
@@ -97,6 +113,11 @@ export default function App() {
             />
           )}
         />
+      </div>
+
+      <div className="col-2">
+        <ProductList title="Products HOC" items={products} />
+        <ProductListWithToggles title="Products HOC" items={products} />
       </div>
     </div>
   );
